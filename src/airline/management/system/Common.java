76e9c5cc68a23042e4eb8d9e9836400aaba28e6a
MoKhalid2001 <mo.khalid2252001@gmail.com>
2022-12-14 17:54:35 +0200

    FirstPush

---
+++ src/airline/management/system/Common.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package airline.management.system;
+
+import java.sql.Connection;
+
+/**
+ *
+ * @author mokhalid
+ */
+public class Common {
+   public static String DBURL = "jdbc:mysql://localhost:3306/AirlineSystem";
+    public static String username = "root";
+     public static Connection Con = null;
+}
